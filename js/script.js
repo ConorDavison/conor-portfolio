150df9757fcb5b8e031186fd063dd0adf40bca0a
ConorDavison <conorjamesdavison@hotmail.co.uk>
2022-01-18 21:17:54 +0000

    added social links and nived nav

--- js/script.js
+++ js/script.js
@@ -41,4 +41,24 @@ $(document).ready(function () {
         return false;
     })
 
-})
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header-area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header-area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header-area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header-area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+});
+
